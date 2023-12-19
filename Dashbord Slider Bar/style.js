f001311292ae0d9acf26a4339a76eecb72581938
Gaurav <gaurav556pant@gmail.com>
2023-12-19 16:07:49 +0530

    updating on a dashbord and working on a text eaditor

---
+++ Dashbord Slider Bar/style.js
@@ -0,0 +1,9 @@
+document.getElementById('menu-icon').addEventListener('click', function () {
+    document.body.classList.toggle('show-menu');
+    document.body.classList.remove('show-hidden-menu');
+});
+
+document.getElementById('close-icon').addEventListener('click', function () {
+    document.body.classList.toggle('show-hidden-menu');
+    document.body.classList.remove('show-menu');
+});
