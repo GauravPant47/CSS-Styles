641e66017d8400f5492e927321c89cab9c9ad4f4
Gaurav <gaurav556pant@gmail.com>
2024-01-07 12:29:39 +0530

    adding on a Dark Mode

---
+++ Dark mode/dist/script.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var darkMode = false;
+
+function toggleDarkMode() {
+  var modeSwitch = document.getElementById('mode-switch');
+  darkMode = !darkMode;
+
+  if (darkMode) {
+    // Toggle dark mode styles
+    document.body.style.backgroundColor = '#222';
+    modeSwitch.style.backgroundColor = '#2c3e50';
+  } else {
+    // Toggle light mode styles
+    document.body.style.backgroundColor = '#999';
+    modeSwitch.style.backgroundColor = '#3498db';
+  } // Update the switch state
+
+
+  modeSwitch.checked = darkMode;
+}
