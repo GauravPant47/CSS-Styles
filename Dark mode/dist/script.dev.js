29dac1719ecbf5d40314fabb4bc07928a609dbe6
Gaurav <gaurav556pant@gmail.com>
2024-01-21 11:59:17 +0530

    Dark Button

--- Dark mode/dist/script.dev.js
+++ Dark mode/dist/script.dev.js
@@ -4,16 +4,21 @@ var darkMode = false;
 
 function toggleDarkMode() {
   var modeSwitch = document.getElementById('mode-switch');
+  var colorChange = document.getElementById('wight_color');
   darkMode = !darkMode;
 
   if (darkMode) {
     // Toggle dark mode styles
     document.body.style.backgroundColor = '#222';
+    document.body.style.color = '#fff';
     modeSwitch.style.backgroundColor = '#2c3e50';
+    colorChange.style.color = '#fff';
   } else {
     // Toggle light mode styles
     document.body.style.backgroundColor = '#999';
+    document.body.color = '000';
     modeSwitch.style.backgroundColor = '#3498db';
+    colorChange.style.color = '#000';
   } // Update the switch state
 
 
--- Dark mode/script.js
+++ Dark mode/script.js
@@ -2,17 +2,22 @@ let darkMode = false;
 
 function toggleDarkMode() {
     const modeSwitch = document.getElementById('mode-switch');
+    const colorChange = document.getElementById('wight_color');
 
     darkMode = !darkMode;
 
     if (darkMode) {
         // Toggle dark mode styles
         document.body.style.backgroundColor = '#222';
+        document.body.style.color = '#fff';
         modeSwitch.style.backgroundColor = '#2c3e50';
+        colorChange.style.color='#fff';
     } else {
         // Toggle light mode styles
         document.body.style.backgroundColor = '#999';
+        document.body.color ='000';
         modeSwitch.style.backgroundColor = '#3498db';
+        colorChange.style.color='#000';
     }
 
     // Update the switch state
